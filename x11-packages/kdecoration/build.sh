289746545db373111d3139885846f8a9e7830ed5
BullyMaguire-lol <temp9040@gmail.com>
2025-07-23 20:50:23 +0530

    build.sh

---
+++ x11-packages/kdecoration/build.sh
@@ -0,0 +1,15 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/plasma/kdecoration"
+TERMUX_PKG_DESCRIPTION="Plugin-based library to create window decorations"
+TERMUX_PKG_LICENSE="LGPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.4.3"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/plasma/${TERMUX_PKG_VERSION}/kdecoration-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="bd0f99bdf487a859da8b1227e3443259ad28e90f110bd3a7bcecc386fded7824"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++, qt6-qtbase, kf6-ki18n"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
