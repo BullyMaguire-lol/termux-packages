2b297547a784d79ed31266e097ffdccdfdae57e5
BullyMaguire-lol <temp9040@gmail.com>
2025-07-25 17:55:25 +0530

    build.sh

---
+++ x11-packages/breeze-gtk/build.sh
@@ -0,0 +1,14 @@
+TERMUX_PKG_HOMEPAGE="https://invent.kde.org/plasma/breeze-gtk"
+TERMUX_PKG_DESCRIPTION="Breeze widget theme for GTK 2 and 3"
+TERMUX_PKG_LICENSE="LGPL-2.0-or-later"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="6.4.3"
+TERMUX_PKG_SRCURL="https://download.kde.org/stable/plasma/${TERMUX_PKG_VERSION}/breeze-gtk-${TERMUX_PKG_VERSION}.tar.xz"
+TERMUX_PKG_SHA256="db91ad2a462596bc715d3091b09e86c79da0060a13c6a0de1b0324ef060ee803"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_BUILD_DEPENDS="breeze, extra-cmake-modules, pycairo, sassc"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DCMAKE_SYSTEM_NAME=Linux
+-DKDE_INSTALL_QMLDIR=lib/qt6/qml
+-DKDE_INSTALL_QTPLUGINDIR=lib/qt6/plugins
+"
