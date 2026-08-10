12517a18496c6a44bcb23ad027118c511678dff1
BullyMaguire-lol <temp9040@gmail.com>
2026-08-10 16:12:22 +0530

    addpkg(main/oboe): 1.10.0

---
+++ packages/oboe/build.sh
@@ -0,0 +1,13 @@
+TERMUX_PKG_HOMEPAGE="https://github.com/google/oboe"
+TERMUX_PKG_DESCRIPTION="Oboe is a C++ library which makes it easy to build high-performance audio apps on Android"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_VERSION="1.10.0"
+TERMUX_PKG_SRCURL="https://github.com/google/oboe/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_SHA256="0e4245f8860c4287040a5d76501c588490bcc9cb57614c486c0c201a5dde3e9f"
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
+-DBUILD_SHARED_LIBS=OFF
+-DCMAKE_POSITION_INDEPENDENT_CODE=ON
+-DOBOE_DISABLE_CONVERSION=OFF
+"
